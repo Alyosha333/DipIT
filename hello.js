bf87aa84d0c6d5ba1a380806757b37c748b2f780
Alyosha333 <alexeiksefteris@protonmail.ch>
2023-11-09 15:14:47 +1100

    modified hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,17 @@
 console.log("Hello guys");
 
 console.log("Hello from local");
+
+/*
+
+Git clone - URL
+
+git add filename.extension - adds file
+
+git commit m- "Message" - commits the additions and adds a message at the end
+
+git status
+git log
+
+git push origin - sends commits to origin / remote
+ */
